b97f9db6d803fc5eec3a490be8b2548f34a1a635
Rohan S Byrne <rohan.byrne@gmail.com>
2019-10-11 20:35:34 +1100

    Changed selector algorithm.

--- selector.py
+++ selector.py
@@ -22,35 +22,19 @@ class Selector:
     def update(self):
         pass
 
-    def exclude(self, cardID):
-        conditions = []
-        conditions.append(cardID in self.history.get_last(5))
-        conditions.append(self.history.latest_lambdas[cardID] < 1.)
-        if any(conditions):
-            return True
-        return False
-
-    def choose_random_card(self, cardList):
+    def retrieve_random_card(self, cardList = None):
+        if cardList is None:
+            cardList = [
+                card \
+                    for card in self.cardIDs.keys() \
+                        if not card in self.history.get_last(5)
+                ]
+            if len(cardList) == 0:
+                print("Choosing random card from all cards.")
+                cardList = self.cardIDs.keys()
         return random.choice(cardList)
 
-    def _select(self):
-        self.update()
-        lambdaList = sorted(
-            self.history.latest_lambdas.items(),
-            key = lambda item: item[1]
-            )[::-1]
-        for cardID, lambdaVal in lambdaList:
-            if not self.exclude(cardID):
-                print("Choosing card based on lambda value.")
-                return cardID
-        unseen_cards = [
-            card \
-                for card in self.cardIDs.keys() \
-                    if not card in self.history.past_cards
-            ]
-        if all(cointosses(1)) and len(unseen_cards) > 0:
-            print("Choosing random card from unseen cards.")
-            return self.choose_random_card(unseen_cards)
+    def retrieve_random_past_card(self):
         legit_past_cards = [
             card \
                 for card in self.history.past_cards \
@@ -58,9 +42,51 @@ class Selector:
             ]
         if len(legit_past_cards) > 0:
             print("Choosing random card from past cards.")
-            return self.choose_random_card(legit_past_cards)
-        print("Choosing random card from all cards.")
-        return self.choose_random_card(self.cardIDs.keys())
+            return self.retrieve_random_card(legit_past_cards)
+        else:
+            return self.retrieve_random_card()
+
+    def retrieve_new_card(self):
+        unseen_cards = [
+            card \
+                for card in self.cardIDs.keys() \
+                    if not card in self.history.past_cards
+            ]
+        if len(unseen_cards) > 0:
+            print("Choosing random card from unseen cards.")
+            return self.retrieve_random_card(unseen_cards)
+        else:
+            return self.retrieve_random_card()
+
+    def retrieve_worst_lambda(self):
+        lambdaList = sorted(
+            self.history.latest_lambdas.items(),
+            key = lambda item: item[1]
+            )
+        cardList = [item[0] for item in lambdaList]
+        cardList = [
+            card \
+                for card in [item[0] for item in lambdaList] \
+                    if not card in self.history.get_last(5)
+            ]
+        if len(cardList) > 0:
+            print("Choosing card based on highest lambda value.")
+            return cardList[-1]
+        else:
+            return self.retrieve_random_past_card()
+
+    def new_card_condition(self):
+        return all(cointosses(3))
+
+    def _select(self):
+        self.update()
+        if self.new_card_condition():
+            return self.retrieve_new_card()
+        else:
+            if cointoss():
+                return self.retrieve_worst_lambda()
+            else:
+                return self.retrieve_random_past_card()
 
     def select(self):
         selectedID = self._select()
